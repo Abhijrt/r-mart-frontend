3d224809319666972f4689ae472e3937d4137907
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-25 17:49:46 +0530

    connecting the store with the index.js main file

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { configureStore } from "./store";
+import { Provider } from "react-redux";
+
+let store = configureStore();
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
